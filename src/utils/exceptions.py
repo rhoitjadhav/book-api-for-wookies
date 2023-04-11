a7d6342f7f84f86b75f2ff4487dfdc2055e1dd8a
rhoitjadhav <jadhavrohit497@gmail.com>
2023-04-11 16:33:22 +0530

    improve code

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -8,10 +8,12 @@ class JWTTokenError(HTTPException):
     """This exception is used for all the jwt token related errors"""
 
     def __init__(
-        self,
-        status_code: int,
-        detail: str,
-        headers: Dict = {"WWW-Authenticate": "Bearer"}
+            self,
+            status_code: int,
+            detail: str,
+            headers=None
     ) -> None:
+        if headers is None:
+            headers = {"WWW-Authenticate": "Bearer"}
 
         super().__init__(status_code=status_code, detail=detail, headers=headers)
